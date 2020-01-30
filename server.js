e89a39689fa8f3f2e2c1de5e82e3a3dbe2e6965f
Rafael Neves <rafael@giver.com.br>
2020-01-30 09:58:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ sockets.on('connection', (socket) => {
 	})
 });
 
-server.listen(3000, () => {
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
 	console.log('> Server listening on port: 3000');
 });
